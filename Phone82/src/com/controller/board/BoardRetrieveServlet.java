8dd6a475384dc59569e3a3e410bca1c59510e785
sohleee <sohlee77@naver.com>
2017-09-11 10:12:24 +0900

    comment

--- Phone82/src/com/controller/board/BoardRetrieveServlet.java
+++ Phone82/src/com/controller/board/BoardRetrieveServlet.java
@@ -1,6 +1,7 @@
 package com.controller.board;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -11,8 +12,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.dto.board.BoardDTO;
+import com.dto.board.CommentDTO;
 import com.exception.MyException;
 import com.service.board.BoardService;
+import com.service.board.CommentService;
 
 @WebServlet("/BoardRetrieveServlet")
 public class BoardRetrieveServlet extends HttpServlet {
@@ -22,10 +25,17 @@ public class BoardRetrieveServlet extends HttpServlet {
 		String num=request.getParameter("num");
 		String target="boardRetrieve.jsp";
 		
+		System.out.println(">"+num);
 		BoardService service=new BoardService();
+		CommentService cservice=new CommentService();
+		HashMap<String,Integer> map=new HashMap<>();
+		map.put("boardnum", Integer.parseInt(num));
+		List<CommentDTO> list=null;
 		try {
+			list=cservice.commentList(map);
 			BoardDTO dto=service.retrieve(Integer.parseInt(num));
 			request.setAttribute("retrieve", dto);
+			request.setAttribute("comment",list);
 		} catch (MyException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
